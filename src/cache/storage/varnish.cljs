ba94372bb0c24a53f3a3a6ec899fc8a391b1f64f
Vinai Kopp <vinai@netzarbeiter.com>
2018-08-17 11:11:04 -0500

    Implement varnish cache storage

---
+++ src/cache/storage/varnish.cljs
@@ -0,0 +1,72 @@
+(ns cache.storage.varnish
+  (:require [cache.storage :as storage]
+            [log.log :as log]))
+
+(defonce http (js/require "http"))
+
+(defonce enabled? (atom true))
+
+(defn- purge-options [server pattern]
+  {:protocol "http:"
+   :hostname (:host server)
+   :port (:port server "80")
+   :method "PURGE"
+   :path "/"
+   :headers {"X-Magento-Tags-Pattern" pattern}
+   :timeout 10000})
+
+(defn request [options success-callback error-callback]
+  (doto (.request http options success-callback)
+    (.on "error" error-callback)
+    (.end)))
+
+(defn- handle-varnish-response [res]
+  (let [status (int (.-statusCode res))]
+    (when-not (= 200 status)
+      (log/debug "Varnish response code" status))
+    (.on res "data" #(when-not (= 200 status)
+                       (log/debug "Response:" %)))))
+
+(defn- handle-varnish-error [e]
+  (log/debug "Varnish request error: " (.-message e))
+  (log/info "No further Varnish PURGEs will be attempted")
+  (reset! enabled? false))
+
+(defn- purge-with-pattern!
+  "Send Varnish PURGE request with the given X-Magento-Tags-Pattern header.
+
+  Reference PHP implementation at
+  \\Magento\\CacheInvalidate\\Model\\PurgeCache::sendPurgeRequest"
+  [server pattern]
+  (let [options (clj->js (purge-options server pattern))]
+    (log/debug "Varnish request:" options)
+    (request options handle-varnish-response handle-varnish-error)))
+
+(defn- tags->pattern
+  "Transform the given tag or tags into a regular expression pattern for use in
+  the X-Magento-Tags-Pattern header.
+
+  Reference PHP implementation at
+  \\Magento\\CacheInvalidate\\Observer\\InvalidateVarnishObserver::execute"
+  [tags]
+  (if (string? tags)
+    (tags->pattern [tags])
+    (apply str (interpose "|" (map #(str "((^|,)" % "(,|$))") (distinct tags))))))
+
+(defn purge-tags! [server tags]
+  (purge-with-pattern! server (tags->pattern tags)))
+
+(defn purge-all! [server]
+  (purge-with-pattern! server ".*"))
+
+(defrecord Varnish [servers]
+  storage/CacheStorage
+
+  (clean-tag [this tag]
+    (run! #(purge-tags! % [tag]) servers))
+
+  (clean-all [this]
+    (run! purge-all! servers)))
+
+(defn create [servers]
+  (->Varnish servers))
