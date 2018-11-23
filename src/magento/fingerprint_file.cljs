7cab550f91bdfe7303ef867e99d89f4f3d25ed32
Vinai Kopp <vinai@netzarbeiter.com>
2018-11-23 10:13:42 +0000

    Build windows path patterns and add more granular cache cleaning for more file types.
    * Specify file path patterns as strings and use string replacement of / to \\
      to make them windows compatible.
    * clean selective cache IDs for events.xml, sections.xml, email_templates.xml
      and ui_component/*.xml files.

--- src/magento/fingerprint_file.cljs
+++ src/magento/fingerprint_file.cljs
@@ -2,10 +2,15 @@
   (:require [file.system :as fs]
             [clojure.string :as string]))
 
-(defn- match-name? [name-pattern file]
-  (cond
-    (regexp? name-pattern) (re-find name-pattern file)
-    :else (= name-pattern (fs/basename file))))
+(defn windowsify [path]
+  (string/replace path "/" "\\\\"))
+
+(defn path-pattern [path-pattern]
+  (re-pattern (cond-> path-pattern
+                (fs/win?) windowsify)))
+
+(defn- match-name? [path-pattern file]
+  (re-find path-pattern file))
 
 (defn- file-fingerprint-fn [name-pattern]
   (fn [file]
@@ -13,74 +18,43 @@
 
 (defn- filenames->fingerprint-fns [type filenames]
   (reduce (fn [acc filename]
-            (assoc acc (file-fingerprint-fn filename) type)) {} filenames))
+            (assoc acc (file-fingerprint-fn (path-pattern filename)) type)) {} filenames))
 
 (defn- config-filetypes []
-  (let [res [#"/etc(?:/[^/]+|)/di\.xml$"
-             #"\\etc(?:\\[^\\]+|)\\di\.xml$"
-             #"/etc/crontab\.xml$"
-             #"\\etc\\crontab\.xml$"
-             #"/etc(?:/[^/]+|)/events\.xml$"
-             #"\\etc(?:\\[^\\]+|)\\events\.xml$"
-             #"/etc/extension_attributes\.xml$"
-             #"\\etc\\extension_attributes\.xml$"
-             #"/etc/(?:[^/]+)/routes\.xml$"
-             #"\\etc\\(?:[^\\]+)\\routes\.xml$"
-             #"/etc/widget\.xml$"
-             #"\\etc\\widget\.xml$"
-             #"/etc/product_types\.xml$"
-             #"\\etc\\product_types\.xml$"
-             #"/etc/product_options\.xml$"
-             #"\\etc\\product_options\.xml$"
-             #"/etc/payment\.xml$"
-             #"\\etc\\payment\.xml$"
-             #"/etc/search_request\.xml$"
-             #"\\etc\\search_request\.xml$"
-             #"/etc/config\.xml$"
-             #"\\etc\\config\.xml$"
-             #"/ui_component/.+\.xml$"
-             #"\\ui_component/.+\.xml$"
-             #"/etc/acl\.xml$"
-             #"\\etc\\acl\.xml$"
-             #"/etc/frontend/sections\.xml$"
-             #"\\etc\\frontend\\sections\.xml$"
-             #"/etc/email_templates\.xml"
-             #"\\etcemail_templates\.xml"
-             #"/etc/adminhtml/system\.xml$"
-             #"\\etc\\adminhtml\\system\.xml$"
-             #"/etc/indexer\.xml$"
-             #"\\etc\\indexer\.xml$"]]
+  (let [res ["/etc(?:/[^/]+|)/di\\.xml$"
+             "/etc/extension_attributes\\.xml$"
+             "/etc/(?:[^/]+)/routes\\.xml$"
+             "/etc/widget\\.xml$"
+             "/etc/product_types\\.xml$"
+             "/etc/product_options\\.xml$"
+             "/etc/payment\\.xml$"
+             "/etc/search_request\\.xml$"
+             "/etc/config\\.xml$"
+             "/etc/acl\\.xml$"
+             "/etc/indexer\\.xml$"]]
     (filenames->fingerprint-fns ::config res)))
 
 (defn- layout-filetypes []
-  (let [res [#"/layout/.+\.xml$"
-             #"\\layout/.+\.xml$"
-             #"/page_layout/.+\.xml$"
-             #"\\page_layout/.+\.xml$"]]
+  (let [res ["/layout/.+\\.xml$"
+             "/page_layout/.+\\.xml$"]]
     (filenames->fingerprint-fns ::layout res)))
 
 (defn- translation-filetypes []
-  (let [res [#"/i18n/.+\.csv$"
-             #"\\i18n/.+\.csv$"]]
+  (let [res ["/i18n/.+\\.csv$"]]
     (filenames->fingerprint-fns ::translation res)))
 
 (defn- template-filetypes []
-  (let [res [#"/templates/.+\.phtml$"
-             #"\\templates/.+\.phtml$"
-             #"/etc/view\.xml$"
-             #"\\etc\\view\.xml$"
-             #"/theme\.xml$"
-             #"\\theme\.xml$"]]
+  (let [res ["/templates/.+\\.phtml$"
+             "/etc/view\\.xml$"
+             "/theme\\.xml$"]]
     (filenames->fingerprint-fns ::template res)))
 
 (defn- requirejs-config-filetypes []
-  (let [res [#"/view/(?:base|frontend|adminhtml)/requirejs-config\.js$"
-             #"\\view\\(?:base|frontend|adminhtml)\\requirejs-config\.js$"]]
+  (let [res ["/view/(?:base|frontend|adminhtml)/requirejs-config\\.js$"]]
     (filenames->fingerprint-fns ::requirejs-config res)))
 
 (defn- menu-filetypes []
-  (let [res [#"/etc/adminhtml/menu\.xml$"
-             #"\\etc\\adminhtml\\menu\.xml$"]]
+  (let [res ["/etc/adminhtml/menu\\.xml$"]]
     (filenames->fingerprint-fns ::menu res)))
 
 (def file->type
@@ -91,14 +65,42 @@
          (menu-filetypes)
          (requirejs-config-filetypes)))
 
-(defn- make-file->ids []
-  (let [res [[#"[/\\]etc[/\\]queue\.xml" ["message_queue_config_cache"]]
-             [#"[/\\]etc[/\\]queue_consumer\.xml" ["message_queue_consumer_config_cache"]]
-             [#"[/\\]etc[/\\]queue_publisher\.xml" ["message_queue_publisher_config_cache"]]
-             [#"[/\\]etc[/\\]queue_topology\.xml" ["message_queue_topology_config_cache"]]]
-        id-fns (map (fn [[re ids]]
-                      (fn [file]
-                        (when (match-name? re file) ids))) res)]
-    (apply some-fn id-fns)))
+(defn- make-ui-component->ids
+  "Return a matcher fn where the returned cache id contains part of the file name."[]
+  (let [ui-comp-pattern (path-pattern "/ui_component/(.+)\\.xml$")]
+    (fn [file]
+      (when-let [m (re-find ui-comp-pattern file)]
+        [(str "ui_component_configuration_data_" (second m))]))))
+
+(defn- make-file->ids
+  "Configure the mapping of file name regex to cache-id's to be cleaned on
+  changes. It would also be possible to clear the complete config cache type
+  for these, but this more granular approach should allow for faster cache
+  rebuild times. If there are problems add them to config-filetypes above."
+  []
+  (let [res [["/etc/adminhtml/system\\.xml$" ["adminhtml__backend_system_configuration_structure"]]
+             ["/etc/queue\\.xml$" ["message_queue_config_cache"]]
+             ["/etc/queue_consumer\\.xml$" ["message_queue_consumer_config_cache"]]
+             ["/etc/queue_publisher\\.xml$" ["message_queue_publisher_config_cache"]]
+             ["/etc/queue_topology\\.xml$" ["message_queue_topology_config_cache"]]
+             ["/etc/crontab\\.xml$" ["crontab_config_cache"]]
+             ["/etc/events\\.xml" ["global__event_config_cache"]]
+             ["/etc/frontend/events\\.xml$" ["frontend__event_config_cache"]]
+             ["/etc/adminhtml/events\\.xml$" ["adminhtml__event_config_cache"]]
+             ["/etc/webapi_rest/events\\.xml$" ["webapi_rest__event_config_cache"]]
+             ["/etc/webapi_soap/events\\.xml$" ["webapi_soap__event_config_cache"]]
+             ["/etc/crontab/events\\.xml$" ["crontab__event_config_cache"]]
+             ["/etc/frontend/sections\\.xml$" ["sections_invalidation_config"]]
+             ["/etc/email_templates\\.xml$" ["email_templates"]]
+             ;; the following need to be tested before removing them from the
+             ;; cbnfig cache type above
+             #_["/etc/acl\\.xml$" ["provider_acl_resources_cache"]]
+             ]
+        id-fns (map (fn [[pattern-string ids]]
+                      (let [re (path-pattern pattern-string)]
+                        (fn [file]
+                          #_(prn pattern-string)
+                          (when (match-name? re file) ids)))) res)]
+    (apply some-fn (conj id-fns (make-ui-component->ids)))))
 
 (def file->ids (make-file->ids))
