3afbc336bcbe073e7f8140b273ccd916d00ff37e
Vinai Kopp <vinai@netzarbeiter.com>
2019-11-19 12:34:15 +0100

    Add support for catalog_attributes.xml changes

--- src/magento/fingerprint_file.cljs
+++ src/magento/fingerprint_file.cljs
@@ -96,8 +96,9 @@
              ["/etc/email_templates\\.xml$" ["email_templates"]]
              ["/etc/webapi\\.xml" ["webapi_config"]]
              ["/etc/schema.graphqls" ["magento_framework_graphqlschemastitching_config_data"]]
+             ["/etc/catalog_attributes\\.xml" ["catalog_attributes"]]
              ;; the following need to be tested before removing them from the
-             ;; cbnfig cache type above
+             ;; config cache type above
              #_["/etc/acl\\.xml$" ["provider_acl_resources_cache"]]
              ]
         id-fns (map (fn [[pattern-string ids]]
